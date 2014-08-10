8a4a2f98d6bd778728920343d209896b2d2732ef
Dane Petersen <thegreatsunra@gmail.com>
2014-08-09 21:47:07 -0700

    Update app.js and link up myUIRouterApp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,4 +4,28 @@
 
 'use strict';
 
-console.log('\'Allo \'Allo!');
+angular.module('myUIRouterApp', [
+  'ui.router',
+  'home'
+])
+.provider('greeter', function() {
+  var salutation = 'Hello';
+  this.setSalutation = function(s) {
+    salutation = s;
+  };
+
+  function Greeter() {
+    this.greet = function(a) {
+      return salutation + ' ' + a;
+    };
+  }
+
+  this.$get = function() {
+    return new Greeter();
+  };
+})
+.config(function($urlRouterProvider, greeterProvider) {
+  $urlRouterProvider
+    .otherwise('/user/notifications');
+  greeterProvider.setSalutation('Ahoy');
+});
